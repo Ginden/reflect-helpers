f65e740b2057ab820f1dcedfcc074d13caf66be4
Michał Wadas <michalwadas@gmail.com>
2015-09-25 12:47:04 +0200

    Another test for _R.makeGeneric

---
+++ tests/make_generic.js
@@ -0,0 +1,20 @@
+/**
+ * Created by michal.wadas on 2015-09-25.
+ */
+
+var _R = typeof require === 'function' ? require('./lib/reflect-helpers.js') : (typeof window !== 'undefined' ? window._R : null);
+
+describe('_R.makeGeneric', function(){
+    it('Checks if Array#slice is properly extracted', function(){
+        var func = _R.makeGeneric([].slice);
+        expect(JSON.stringify(func('abcd'))).toEqual(JSON.stringify('abcd'.split('')));
+    });
+
+    it('Checks if Object#toString is properly extracted', function(){
+        var func = _R.makeGeneric(Object.prototype.toString);
+        expect(func('xd'))
+            .toEqual(({}).toString.call('xd'));
+    });
+
+
+});
